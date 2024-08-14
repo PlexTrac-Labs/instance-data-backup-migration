1702bf2912749da98a84af74a4fe4d50a8e4e80a
Peter Green <peter.green@plextrac.com>
2024-08-14 17:18:20 -0600

    add option to Reports workflow to change how reports are imported
    added second method to import reports that doesn't require selecting an existing client to import to. This method involves checking the client name on the saved ptrac and checking if there is an existing client with the same in the Plextrac instance. If so, the report will be imported under this client. If not, a new client will be created with just the client name in the ptrac file and the report will be imported under this new client.

--- workflows/reports.py
+++ workflows/reports.py
@@ -3,6 +3,7 @@ import os
 import json
 import io
 from typing import Union
+from copy import deepcopy
 
 from tkinter import Tk
 from tkinter.filedialog import askopenfilenames
@@ -88,7 +89,19 @@ class ReportsWorkflow:
         print(f'''[b u]Reports Workflow[/b u]
 
 This workflow can import/export reports from Plextrac. Reports will be exported
-as PTRAC files, which can be reimported to a client later.
+as PTRAC files, which can be reimported later.
+              
+[b]IMPORTANT:[/b] Reports can be imported in 2 fundamentally different ways.
+          
+First reports can be bulk imported to a single manually selected client of your choice.
+The client to import to must be an existing client in your Plextrac instance.
+              
+The second choice is an automatic approach. You don't select a client and each report
+tries to find the client it was attached to when exported. The original client name is
+saved in the PTRAC on export. This import method uses that client name to look for an
+existing client in your instance with the same name. If found the report will be added
+under that client. Otherwise a new client will be created with the client name saved on
+the PTRAC file, and the report imported under this new client.
 
 Overview of Steps:
 - Export reports
@@ -97,8 +110,9 @@ Overview of Steps:
     user data, report Operators and Reviewers will be stripped before files are saved
 - Import reports
   - select PTRAC files of reports to import
-  - select an existing client in Plextrac to import reports to
-  - choose whether to check if the client exists
+  - select manual vs automatic approach for importing reports
+    - select an existing client in Plextrac to import reports to
+    - automatically import reports
               
 [b]Would you like to import or exports reports[/b]''')
         action = binput.select([":import reports", ":export reports", ":main menu"], cursor=">", cursor_style='white')
@@ -186,6 +200,24 @@ Overview of Steps:
         print(f'Selected {len(ptrac_file_paths)} PTRAC file(s)\n')
         log.debug(f'selected {len(ptrac_file_paths)} PTRAC file(s)')
 
+        # select import method - manual vs automatic
+        print("[b]Select method of importing reports[/b]")
+        action = binput.select([":import reports to single client", ":import reports automatically to multiple clients"], cursor=">", cursor_style='white')
+        print(f'Selected {action[1:]}\n')
+        log.debug(f'selected: {action}')
+
+        method = None
+        if action == ":import reports to single client":
+            print(f'All selected PTRACs will be imported to create a new report under the selected client.')
+            method = "manual"
+        elif action == ":import reports automatically to multiple clients":
+            print(f'All selected PTRACs will be imported under clients depending on client name in PTRAC file.')
+            method = "automatic"
+        else:
+            log.exception(f'Selected invalid option. Exiting to main menu')
+            input(f'Press enter to continue...')
+            main.start()
+
         # get clients from instance
         spinner = binput.spinners.Spinner(binput.spinners.DOTS, "Loading clients from instance...")
         spinner.start()
@@ -197,22 +229,23 @@ Overview of Steps:
             input(f'Press enter to continue...')
             main.start()
 
-        # user selects clients
-        print(f'[b]Loaded Client list:[/b]')
-        selected_client = binput.select(
-            options=clients,
-            # need to replace square brackets with something else since beaupy uses square brackets to define styles, effectively excluding them from allowed chars
-            preprocessor=lambda client:f'Name: {client["name"]} | ID: {client["client_id"]} | Tags: {str(client.get("tags", [])).replace("[","<").replace("]", ">")}',
-            cursor_style='white',
-            pagination=True,
-            page_size=10
-        )
-        print(f'Selected \'{selected_client["name"]}\'\n')
+        # MANUAL METHOD - user selects clients
+        if method == "manual":
+            print(f'[b]Loaded Client list:[/b]')
+            selected_client = binput.select(
+                options=clients,
+                # need to replace square brackets with something else since beaupy uses square brackets to define styles, effectively excluding them from allowed chars
+                preprocessor=lambda client:f'Name: {client["name"]} | ID: {client["client_id"]} | Tags: {str(client.get("tags", [])).replace("[","<").replace("]", ">")}',
+                cursor_style='white',
+                pagination=True,
+                page_size=10
+            )
+            print(f'Selected \'{selected_client["name"]}\'\n')
         
         # import data from report PTRACs to selected client
-        print(f'All selected PTRACs will be imported to create a new report under the selected client.')
         metrics = IterationMetrics(len(ptrac_file_paths))
         for file_path in ptrac_file_paths:
+            log.info(f'Processing PTRAC file \'{file_path}\'')
             # load ptrac dict from file
             ptrac = self.load_data_from_report_PTRAC(file_path)
             if ptrac == None:
@@ -222,6 +255,34 @@ Overview of Steps:
 
             # TODO - add ability to add report tags at this step
 
+            # get client to import to
+            client_id = None
+            if method == "manual":
+                client_id = selected_client['client_id']
+            if method == "automatic":
+                # check need to create new client
+                need_to_create_client = True
+                matching_clients = list(filter(lambda client: ptrac["client_info"]["name"] == client["name"], clients))
+                if len(matching_clients) > 0:
+                    selected_client = matching_clients[0]
+                    client_id = matching_clients[0]['client_id']
+                    log.info(f'Found existing client \'{selected_client["name"]}\'')
+                    need_to_create_client = False
+
+                # create client if it doesn't already exist
+                if need_to_create_client:
+                    log.info(f'Creating new client with name \'{ptrac["client_info"]["name"]}\'')
+                    payload = {"name": ptrac["client_info"]["name"]}
+                    try:
+                        response = api.clients.create_client(globals.auth.base_url, globals.auth.get_auth_headers(), payload)
+                        selected_client = response.json
+                        client_id = response.json['client_id']
+                        log.success(f'Created client \'{payload["name"]}\'')
+                    except Exception as e:
+                        log.exception(f'Could not create client. Skipping client ptrac...')
+                        log.info(metrics.print_iter_metrics())
+                        continue
+
             # import ptrac
             try:
                 json_str = json.dumps(ptrac)
@@ -229,7 +290,7 @@ Overview of Steps:
                 multipart_form_data = {
                     'file': json_file_like
                 }
-                response = api.reports.import_ptrac_report(globals.auth.base_url, globals.auth.get_auth_headers(), selected_client['client_id'], multipart_form_data)
+                response = api.reports.import_ptrac_report(globals.auth.base_url, globals.auth.get_auth_headers(), client_id, multipart_form_data)
                 log.success(f'Created report \'{ptrac["report_info"]["name"]}\' for client \'{selected_client["name"]}\'')
             except Exception as e:
                 log.exception(f'Could not create report. Skipping...')
